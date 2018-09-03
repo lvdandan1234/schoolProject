26d8dcbf0db51eb26e0d7b933ab35772c0767a4b
lvdandan1234 <872716430@qq.com>
2018-09-03 13:02:09 +0800

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
-
-
+import Shopping from '@/components/Shopping'
+import ProDetails from '@/components/ProDetails'
+import FillOrder from '@/components/FillOrder'
+import ShopCart from '@/components/ShopCart'
 Vue.use(Router)
 
 export default new Router({
@@ -17,6 +19,22 @@ export default new Router({
       path: '/index/:id',
       name: 'Hello',
       component: Hello
+    },{
+     path:'/shopping/',
+     name:'Shopping',
+      component:Shopping
+    },{
+      path:'/proDetails/',
+      name:'proDetails',
+      component:ProDetails
+    },{
+      path:'/FillOrder/',
+      name:'FillOrder',
+      component:FillOrder
+    },{
+      path:'/ShopCart/',
+      name:'ShopCart',
+      component:ShopCart
     }
   ]
 });
